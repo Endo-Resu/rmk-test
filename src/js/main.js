d86cf47a6890692dd2662832f1b554b7cc12449f
schiZis <stahanovgod@gmail.com>
2022-02-28 01:48:13 +0500

    near done

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,18 @@
-console.log('main');
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  slidesPerView: 'auto',
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 'auto',
+    },
+    768: {
+      slidesPerView: 'auto',
+    }
+  },
+});
 
+console.log('main');
